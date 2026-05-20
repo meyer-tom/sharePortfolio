9b06bfad5d843adf8cb4d0cb892b95928939e184
loispacqueteau <loispacqueteau@gmail.com>
2026-05-20 16:14:23 +0200

    add exception and add Test

--- src/test/java/fr/utc/miage/shares/ActionSimpleTest.java
+++ src/test/java/fr/utc/miage/shares/ActionSimpleTest.java
@@ -26,21 +26,28 @@ import org.junit.jupiter.api.Test;
 class ActionSimpleTest {
 
     private static final String FOO_SHARE1 = "Foo Share 1";
+    private static final String FOO_EMPTY = "";
     private static final Jour JOUR_1 = new Jour(2026, 1);
     private static final int VALEUR = 10;
     private static final int VALEUR_0 = 0;
 
     @Test
-    void testAllConstructorUsage() {
+    void testConstructor() {
         Assertions.assertDoesNotThrow(() -> {
             new ActionSimple(FOO_SHARE1);
         });
     }
 
     @Test
+    void testConstructorWithoutLabel(){
+        Assertions.assertThrows(IllegalArgumentException.class,() -> {
+            new ActionSimple(FOO_EMPTY);
+        });
+    }
+
+    @Test
     void testEnrgCoursShouldWork() {
         final ActionSimple action = new ActionSimple(FOO_SHARE1);
-
         Assertions.assertDoesNotThrow(() -> {
             action.enrgCours(JOUR_1, VALEUR);
         });
