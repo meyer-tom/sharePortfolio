2733d9b83b2ddad2b1e4ee49b2d6a753337ff0bb
loispacqueteau <loispacqueteau@gmail.com>
2026-05-20 15:02:58 +0200

    Ajout début des tests

--- src/test/java/fr/utc/miage/shares/ActionSimpleTest.java
+++ src/test/java/fr/utc/miage/shares/ActionSimpleTest.java
@@ -1,25 +1,58 @@
 package fr.utc.miage.shares;
 
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
+
+import fr.utc.miage.shares.Jour;
 
 
 class ActionSimpleTest {
 
     private static final String FOO_SHARE1 = "Foo Share 1";
     private static final String FOO_SHARE2 = "Foo Share 2";
+    private static final Jour JOUR_1 =  new Jour(2026, 1);
+    private static final int VALEUR =  10;
+
 
     @Test
-    void testInitializedConstructor() {
-        assertDoesNotThrow(new ActionSimple(FOO_SHARE1));
-        
-       
+    void testAllConstructorUsage() {
+        Assertions.assertDoesNotThrow(() -> {new ActionSimple(FOO_SHARE1);});
     }
 
+    @Test
+    void testenrgCoursShouldWork(){
+        final ActionSimple action = new ActionSimple(FOO_SHARE1);
 
-    
+        Assertions.assertDoesNotThrow(() -> {action.enrgCours(JOUR_1, VALEUR);});
+    }
+
+    @Test
+    void testValeurShouldWork(){
+        final ActionSimple action = new ActionSimple(FOO_SHARE1);
+
+        action.enrgCours(JOUR_1, VALEUR);
+
+        assertEquals(VALEUR,action.valeur(JOUR_1));
+        
+    }
+
+    @Test
+    void testValeurShouldNotWork(){
+        final ActionSimple action = new ActionSimple(FOO_SHARE1);
+
+        action.enrgCours(JOUR_1, VALEUR);
+
+        assertEquals(VALEUR,action.valeur(JOUR_1));
+        
+    }
 
 }
