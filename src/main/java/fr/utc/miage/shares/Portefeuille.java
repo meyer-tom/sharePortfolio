d281e60b3125f62a25729d8f1c51fece7f7da239
Selim-Hamza <selimsoccer7@gmail.com>
2026-05-26 16:27:35 +0200

    ajout test portefeuil

--- src/main/java/fr/utc/miage/shares/Portefeuille.java
+++ src/main/java/fr/utc/miage/shares/Portefeuille.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2026 Tom Meyer.
+ * Copyright 2026 Tom Meyer/Selim Hamza.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
